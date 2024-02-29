71600f8012a3f99eec3813e4a181355f00f2434d
Justin Farley <justin.farley94@gmail.com>
2024-02-29 01:38:05 -0600

    Update orders/page.tsx
    Started the table for displaying the orders. Will continue tomorrow.

--- frontend/app/orders/page.tsx
+++ frontend/app/orders/page.tsx
@@ -1,3 +1,53 @@
+import * as React from 'react';
+
+// We will change this later on to our expected Order data
+interface Data {
+    id: number;
+    calories: number;
+    carbs: number;
+    fat: number;
+    name: string;
+    protein: number;
+}
+
+type Order = 'asc' | 'desc';
+
+interface EnhancedTableProps {
+    numSelected: number;
+    onRequestSort: (event: React.MouseEvent<unknown>, property: keyof Data) => void;
+    onSelectAllClick: (event: React.ChangeEvent<HTMLInputElement>) => void;
+    order: Order;
+    orderBy: string;
+    rowCount: number;
+}
+
+function EnhancedTableHead(props: EnhancedTableProps) {
+    const { onSelectAllClick, order, orderBy, numSelected, rowCount, onRequestSort } = props;
+
+    const createSortHandler = ( property: keyof Data ) => (event: React.MouseEvent<unknown>) => {
+        onRequestSort(event, property);
+    }
+
+    return (
+        <TableHead>
+            <TableRow>
+                <TableCell padding="checkbox">
+                    <Checkbox
+                        color="primary"
+                        indeterminate={numSelected > 0 && numSelected < rowCount }
+                        checked={rowCount > 0 && numSelected === rowCount }
+                        onChange={onSelectAllClick}
+                        inputProps={{
+                            'aria-label': 'select all desserts',
+                        }}
+                    />
+                </TableCell>
+                
+            </TableRow>
+        </TableHead>
+    )
+}
+
 export default function Order() {
     return (
         <>
